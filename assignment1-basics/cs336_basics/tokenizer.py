57aea66ce3c1c245589ec25ec73e1c938ba76465
ChenXinjie04 <xinjiechen04@gmail.com>
2026-04-30 22:56:57 +0800

    tokenizer speed up again

--- assignment1-basics/cs336_basics/tokenizer.py
+++ assignment1-basics/cs336_basics/tokenizer.py
@@ -17,6 +17,10 @@ class Tokenizer:
         self.special_tokens = special_tokens
         self.pair_dict = {merge: idx for idx, merge in enumerate(merges)}
         self.end_of_token = len(vocab)
+        special_tokens = sorted(self.special_tokens, key=len, reverse=True)
+        special_tokens = [regex.escape(special_token) for special_token in special_tokens]
+        self.split_pattern = "(" + "|".join(special_tokens) + ")"
+        self._merge_cache = {}
 
     @classmethod
     def from_files(cls, filepath, special_tokens=None):
@@ -26,10 +30,7 @@ class Tokenizer:
 
     def encode(self, text: str) -> list[int]:
         if self.special_tokens:
-            special_tokens = sorted(self.special_tokens, key=len, reverse=True)
-            special_tokens = [regex.escape(special_token) for special_token in special_tokens]
-            split_pattern = "|".join(special_tokens)
-            splits = regex.split("(" + split_pattern + ")", text)
+            splits = regex.split(self.split_pattern, text)
         else:
             splits = [text]
         pattern = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
@@ -41,7 +42,8 @@ class Tokenizer:
             words = regex.findall(pattern, chunck)
             for word in words:
                 bytes_word = self._merge_word(word)
-                ans = ans + [self.bytes_to_id[word] for word in bytes_word]
+                ans.extend([self.bytes_to_id[word] for word in bytes_word])
+        self._merge_cache.clear()
         return ans
 
     def decode(self, ids: list[int]) -> str:
@@ -55,20 +57,24 @@ class Tokenizer:
             yield from self.encode(line)
 
     def _merge_word(self, word):
+        if word in self._merge_cache:
+            return self._merge_cache[word]
         bytes_word = word.encode()
         bytes_list = [bytes_word[i : i + 1] for i in range(len(bytes_word))]
+        end_of_token = self.end_of_token
+        pair_dict = self.pair_dict
         while True:
             new_bytes_list = []
             merge_pair = ()
-            min_id = self.end_of_token
+            min_id = end_of_token
             for i in range(len(bytes_list) - 1):
                 cur_pair = (bytes_list[i], bytes_list[i + 1])
-                if cur_pair not in self.pair_dict:
+                if cur_pair not in pair_dict:
                     continue
-                if self.pair_dict[cur_pair] < min_id:
-                    min_id = self.pair_dict[cur_pair]
+                if pair_dict[cur_pair] < min_id:
+                    min_id = pair_dict[cur_pair]
                     merge_pair = cur_pair
-            if min_id == self.end_of_token:
+            if min_id == end_of_token:
                 break
             i = 0
             while i < len(bytes_list):
@@ -79,4 +85,5 @@ class Tokenizer:
                     new_bytes_list.append(bytes_list[i])
                     i += 1
             bytes_list = new_bytes_list
+        self._merge_cache[word] = bytes_list
         return bytes_list
--- assignment1-basics/scripts/tokenize_tinystories.py
+++ assignment1-basics/scripts/tokenize_tinystories.py
@@ -1,9 +1,10 @@
 from cs336_basics.tokenizer import Tokenizer
-import pickle
+import time
 
 if __name__ == "__main__":
+    start = time.perf_counter()
     tokenizer = Tokenizer.from_files("./tokenizers/tokenizer_tiny_stories.pkl")
-    with open("./data/tinystories_sample_1M.txt", encoding="utf-8") as f:
+    with open("./data/tinystories_sample_5M.txt", encoding="utf-8") as f:
         text = f.read()
     ids = tokenizer.encode(text)
-    print(ids)
+    print("total time", time.perf_counter() - start)
